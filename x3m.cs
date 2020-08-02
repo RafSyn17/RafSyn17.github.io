7c388ca831497a662cdb87482a071ac9b4d51e26
RafSyn17 <RafSyn17@users.noreply.github.com>
2020-08-02 12:08:30 +0200

    Create x3m.cs

---
+++ x3m.cs
@@ -0,0 +1,31 @@
+body {
+  background: white; /* try type yellow */
+  color: #323232;
+
+  margin: 0;
+  height: 100vh;
+
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  font-family: Helvetica neue, roboto;
+}
+
+th,
+td,
+p,
+input {
+  font: 14px Verdana;
+}
+table,
+th,
+td {
+  border: solid 1px #DDD;
+  border-collapse: collapse;
+  padding: 2px 3px;
+  text-align: center;
+}
+th {
+  font-weight: bold;
+}
